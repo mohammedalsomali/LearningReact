08413987a3e488aca5a291820efb0a43215d857f
mohammedalsomali <d.j.hamoody@gmail.com>
2021-09-06 12:42:22 -0500

    creating a new app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import ToDoList from "./ToDoList";
 
 function App() {
   return (
+    <>
     <ToDoList/>
+    <input type="text" placeholder="add to your list"/>
+    <button> Add to the list</button>
+    <button>clear the list</button>
+    </>
   )
 }
 
