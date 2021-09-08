f70f8a41ef1a31276fbf0a11b253b572a68e6932
mohammedalsomali <d.j.hamoody@gmail.com>
2021-09-07 22:16:43 -0500

    creating a new react app

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -4,6 +4,7 @@ export default function ToDoList() {
     return (
         <div>
             To Do List:
+            <div style={{height: '400px', width: '400px', backgroundColor: 'red', float: 'right'}}> </div>
         </div>
     )
 }
