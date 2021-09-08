e67df21d4bcd00d03ceb627948fdbef2f4c48177
mohammedalsomali <d.j.hamoody@gmail.com>
2021-09-08 18:46:46 -0500

    starting from scratch

--- src/App.js
+++ src/App.js
@@ -1,58 +1,34 @@
-import react from "react";
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
 
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useCollectionData } from 'react-firebase-hooks/firestore';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 
-firebase.initializeApp({
-  //your config
-  apiKey: "AIzaSyBPfec-2EUFl1F6uHNsq6c7gd_js8Mgw7I",
-  authDomain: "chatapp-deee3.firebaseapp.com",
-  projectId: "chatapp-deee3",
-  storageBucket: "chatapp-deee3.appspot.com",
-  messagingSenderId: "749671813134",
-  appId: "1:749671813134:web:678ab0c09ea152b68a2c5f",
-  measurementId: "G-5EYGJLLGY3"
-})
-
-const [user] = useAuthState(auth);
 
 
-import ToDoList from "./ToDoList";
+
 
 function App() {
+
+
   return (
-    <div className='App'>
+    <div style={{backgroundColor: "red", height: '500px', width: '350px' }}>
       <header>
-
-
+        <h1>⚛️🔥💬</h1>
+       
       </header>
 
-    <section> 
-      {user ? <ChatRoom /> : <SingIn/> }
-    </section>
+      <section>
+       <textarea type='textbox'/>
+      </section>
+      <div>
+      <input type='text'/>
+
+        <button> send </button>
+      </div>
+
     </div>
-  )
+  );
 }
 
 
-function SingIn() {
-  const singInWithGoogle = () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithPopup(provider);
-  }
-  return (
-    <button onClick= {singInWithGoogle }> sing in with Google </button>
-  )
-}
-
-
-function SignOut() {
-  return auth.currentUser && (
-    <button onClick= { () => auth.signOut() }> Sign out</button>
-  )
-}
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.js';
-
+import App from './App';
 
 ReactDOM.render(
-  <App />,
+  
+    <App />,
   document.getElementById('root')
 );
-
 
