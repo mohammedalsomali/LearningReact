39fecb678457cfee515ac4d10eb2f14d267bad9c
mohammedalsomali <d.j.hamoody@gmail.com>
2021-09-09 21:03:22 -0500

    finished the to do list functionality

--- src/App.js
+++ src/App.js
@@ -8,23 +8,39 @@ import ReactDOM from 'react-dom';
 
 
 function App() {
+  function Addtodo(e) {
+    const task = e.target.elements
+    const div1 = document.querySelector('#div')
+    let elemt = document.createElement('div')
+    let check = document.createElement('input')
+    check.type = 'checkbox'
+    elemt.innerHTML = task.input.value
+    elemt.appendChild(check)
+
+    div1.appendChild(elemt)
+    task.input.value = ''
+    e.preventDefault()
+
+
+  }
 
 
   return (
-    <div style={{backgroundColor: "red", height: '500px', width: '350px' }}>
+    <div style={{ backgroundColor: "red", height: '500px', width: '350px'}} >
       <header>
         <h1>⚛️🔥💬</h1>
-       
+
       </header>
 
-      <section>
-       <textarea type='textbox'/>
-      </section>
-      <div>
-      <input type='text'/>
+      <ul id='div' style={{ width: '88.5%', height: '70%', backgroundColor: 'darkcyan',overflowY: 'scroll', scrollbar: 'None' }}>
+      </ul>
+      
+      <form onSubmit={Addtodo}>
 
-        <button> send </button>
-      </div>
+        <input type='text' id='input' />
+
+        <button type='submit' > send </button>
+      </form>
 
     </div>
   );
@@ -32,3 +48,5 @@ function App() {
 
 
 export default App;
+
+
